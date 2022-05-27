613b4bab7c25b24724ac989d8ae606c5c7fa09ba
Roja Kaveh Garna <51385233+Rojakaveh@users.noreply.github.com>
2022-05-27 17:22:41 -0400

    Update LOOCV.R

--- LOOCV.R
+++ LOOCV.R
@@ -1,7 +1,4 @@
-#create data frame
-df <- data.frame(y=c(6, 8, 12, 14, 14, 15, 17, 22, 24, 23),
-                 x1=c(2, 5, 4, 3, 4, 6, 7, 5, 8, 9),
-                 x2=c(14, 12, 12, 13, 7, 8, 7, 4, 6, 5))
+
 Sys.unsetenv("http_proxy"); Sys.unsetenv("https_proxy")
 
 ############################################## #LITTLE OTTER CK AT MONKTON RD.
